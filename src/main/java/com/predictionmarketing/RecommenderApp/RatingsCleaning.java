a916c507dfd2d415df7ce4888a987a17ebdbedd6
hungpv <hungpv@elcom.com.vn>
2016-05-30 11:37:43 +0700

    add readme

--- src/main/java/com/predictionmarketing/RecommenderApp/RatingsCleaning.java
+++ src/main/java/com/predictionmarketing/RecommenderApp/RatingsCleaning.java
@@ -27,5 +27,35 @@ public class RatingsCleaning {
 		}
 		br.close();
 		bw.close();
+		
+		// If using ml-latest FULL
+		/*
+		File input = new File("C:/Users/hungpv/Desktop/ratings.dat");
+		File output = new File("C:/Users/hungpv/Desktop/dataset.dat");
+
+		FileReader fr = new FileReader(input);
+		BufferedReader br = new BufferedReader(fr);
+		// this will read the first line
+		//br.readLine();
+		FileWriter fw = new FileWriter(output);
+		BufferedWriter bw = new BufferedWriter(fw);
+
+		String line;
+		while ((line = br.readLine()) != null) {
+			StringBuilder sb = new StringBuilder();
+			String[] parts = line.split("::");
+			for (int i = 0; i < parts.length - 1; i++) {
+				sb.append(parts[i] + ",");
+
+			}
+
+			bw.write(sb.substring(0, sb.length() - 1));
+			bw.newLine();
+
+		}
+		bw.close();
+		System.out.println("done!");
+		*/
+		
 	}
 }
